750776a98cf659f93e50b8212b54ce4da6da0684
Madushan Nishantha <j.l.madushan@gmail.com>
2020-01-26 22:39:43 +0530

    not working

--- src/handshake.rs
+++ src/handshake.rs
@@ -1,62 +1,50 @@
-use libsodium_sys::{crypto_sign_ed25519_pk_to_curve25519, crypto_sign_ed25519_sk_to_curve25519};
-pub use scalarmult::GroupElement as SharedSecret;
-use sodiumoxide::crypto::scalarmult::{self, scalarmult, Scalar};
-
 use crate::*;
 
-pub use box_::{PublicKey as EphPublicKey, SecretKey as EphSecretKey};
-use sodiumoxide::crypto::box_;
-
 pub struct HandshakeKeys {
-    pub read_key: secretbox::Key,
+    pub read_key: [u8; 32],
     pub read_noncegen: NonceGen,
 
-    pub write_key: secretbox::Key,
+    pub write_key: [u8; 32],
     pub write_noncegen: NonceGen,
 }
 
-pub fn generate_ephemeral_keypair() -> (EphPublicKey, EphSecretKey) {
-    box_::gen_keypair()
+#[wasm_bindgen]
+pub struct  EphKeyPair {
+    publicKey: [u8; 32],
+    privateKey: [u8; 32],
+    keyType: String,
+}
+pub fn generate_ephemeral_keypair() -> ([u8; 32], [u8; 32]) {
+    let ephkeypair = crypto_box_keypair();
+    (ephkeypair.publicKey, ephkeypair.privateKey)
 }
 
 pub fn derive_shared_secret(
-    our_sec: &EphSecretKey,
-    their_pub: &EphPublicKey,
-) -> Option<SharedSecret> {
+    our_sec: &[u8; 32],
+    their_pub: &[u8; 32],
+) -> Option<[u8; 32]> {
     // Benchmarks suggest that these "copies" get optimized away.
-    let n = Scalar::from_slice(&our_sec[..])?;
-    let p = SharedSecret::from_slice(&their_pub[..])?;
-    scalarmult(&n, &p).ok()
+    let mut buf = [0u8; 32];
+    crypto_scalarmult(Uint8Array::from(our_sec as &[u8]), Uint8Array::from(their_pub as &[u8])).copy_to(&mut buf);
+    Some(buf)
 }
 
-pub fn derive_shared_secret_pk(sk: &EphSecretKey, pk: &PublicKey) -> Option<SharedSecret> {
+pub fn derive_shared_secret_pk(sk: &[u8; 32], pk: &[u8; 32]) -> Option<[u8; 32]> {
     pk_to_curve(&pk).and_then(|c| derive_shared_secret(&sk, &c))
 }
 
-pub fn derive_shared_secret_sk(sk: &SecretKey, pk: &EphPublicKey) -> Option<SharedSecret> {
+pub fn derive_shared_secret_sk(sk: &[u8; 64], pk: &[u8; 32]) -> Option<[u8; 32]> {
     sk_to_curve(&sk).and_then(|c| derive_shared_secret(&c, &pk))
 }
 
-fn pk_to_curve(k: &PublicKey) -> Option<EphPublicKey> {
-    let mut buf = [0; size_of::<EphPublicKey>()];
-
-    let ok = unsafe { crypto_sign_ed25519_pk_to_curve25519(buf.as_mut_ptr(), k.0.as_ptr()) == 0 };
-
-    if ok {
-        EphPublicKey::from_slice(&buf)
-    } else {
-        None
-    }
+fn pk_to_curve(k: &[u8; 32]) -> Option<[u8; 32]> {
+    let mut buf = [0u8; 32];
+    crypto_sign_ed25519_pk_to_curve25519(Uint8Array::from(k as &[u8])).copy_to(&mut buf);
+    Some(buf)
 }
 
-fn sk_to_curve(k: &SecretKey) -> Option<EphSecretKey> {
-    let mut buf = [0; size_of::<EphSecretKey>()];
-
-    let ok = unsafe { crypto_sign_ed25519_sk_to_curve25519(buf.as_mut_ptr(), k.0.as_ptr()) == 0 };
-
-    if ok {
-        EphSecretKey::from_slice(&buf)
-    } else {
-        None
-    }
+fn sk_to_curve(k: &[u8; 64]) -> Option<[u8; 32]> {
+    let mut buf = [0u8; 32];
+    crypto_sign_ed25519_sk_to_curve25519(Uint8Array::from(k as &[u8])).copy_to(&mut buf);
+    Some(buf)
 }
--- src/lib.rs
+++ src/lib.rs
@@ -1,77 +1,92 @@
+use wasm_bindgen::prelude::*;
 use core::mem::size_of;
-use std::sync::Once;
+use ::std::convert::{TryInto};
+use js_sys::{Promise, Uint8Array};
+use handshake::EphKeyPair;
 
-extern crate sodiumoxide;
-use sodiumoxide::crypto::{auth, sign};
 
-pub use auth::Tag as AuthTag;
-pub use sign::{
-    sign_detached, verify_detached, PublicKey, SecretKey, Signature, PUBLICKEYBYTES,
-    SECRETKEYBYTES, SIGNATUREBYTES,
-};
-pub use sodiumoxide::crypto::secretbox;
+#[wasm_bindgen(raw_module = "src/js/sodium.js")]
+extern "C" {
+    type libsodium_sumo;
+    fn ready() -> Promise;
+    fn crypto_sign_keypair() -> KeyPair;
+    fn randombytes_buf(len: usize) -> Uint8Array;
+    fn crypto_auth(msg: &[u8], key: &Uint8Array) -> Uint8Array;
+    fn crypto_auth_verify(mac: Uint8Array, msg: &[u8], key: &Uint8Array) -> bool;
+    fn crypto_box_keypair() -> EphKeyPair;
+    fn crypto_scalarmult(privateKey: Uint8Array, publicKey: Uint8Array) -> Uint8Array;
+    fn crypto_sign_ed25519_pk_to_curve25519(publicKey: Uint8Array) -> Uint8Array;
+    fn crypto_sign_ed25519_sk_to_curve25519(privateKey: Uint8Array) -> Uint8Array;
+}
+
+#[wasm_bindgen]
+pub struct KeyPair {
+    publicKey: [u8; 32],
+    privateKey: [u8; 64],
+    keyType: String
+}
 
 pub mod handshake;
 pub mod hash;
 pub mod utils;
 
-static INIT: Once = Once::new();
-pub fn init() {
-    INIT.call_once(|| {
-        sodiumoxide::init().expect("Failed to initialize libsodium.");
-    });
+pub async fn init() {
+    wasm_bindgen_futures::JsFuture::from(ready()).await.unwrap();
 }
 
-pub fn generate_longterm_keypair() -> (PublicKey, SecretKey) {
-    sign::gen_keypair()
+pub fn generate_longterm_keypair() -> ([u8; 32], [u8; 64]) {
+    let keypair = crypto_sign_keypair();
+    (keypair.publicKey, keypair.privateKey)
 }
 
 /// 32-byte network key, known by client and server. Usually `NetworkKey::SSB_MAIN_NET`
 #[derive(Clone, Debug, PartialEq)]
-pub struct NetworkKey(auth::Key);
+pub struct NetworkKey([u8; 32]);
 impl NetworkKey {
-    pub const SSB_MAIN_NET: NetworkKey = NetworkKey(auth::Key([
+    pub const SSB_MAIN_NET: NetworkKey = NetworkKey([
         0xd4, 0xa1, 0xcb, 0x88, 0xa6, 0x6f, 0x02, 0xf8, 0xdb, 0x63, 0x5c, 0xe2, 0x64, 0x41, 0xcc,
         0x5d, 0xac, 0x1b, 0x08, 0x42, 0x0c, 0xea, 0xac, 0x23, 0x08, 0x39, 0xb7, 0x55, 0x84, 0x5a,
         0x9f, 0xfb,
-    ]));
+    ]);
 
     pub fn random() -> NetworkKey {
-        let mut buf = [0u8; NetworkKey::size()];
-        utils::randombytes_into(&mut buf);
-        NetworkKey::from_slice(&buf).unwrap()
+        let mut buf = randombytes_buf(NetworkKey::size());
+        NetworkKey::from_slice(&buf.to_vec()).unwrap()
     }
 
     pub fn as_slice(&self) -> &[u8] {
         &self.0[..]
     }
     pub fn from_slice(b: &[u8]) -> Option<NetworkKey> {
-        Some(NetworkKey(auth::Key::from_slice(b)?))
+        Some(NetworkKey(b.try_into().expect("incorrect length")))
     }
 
-    pub fn authenticate(&self, data: &[u8]) -> AuthTag {
-        auth::authenticate(data, &self.0)
+    pub fn authenticate(&self, data: &[u8]) -> [u8; 32] {
+        let mut buf = [0u8; 32];
+        crypto_auth(data, &Uint8Array::from(&self.0 as &[u8])).copy_to(&mut buf);
+        buf
     }
 
-    pub fn verify(&self, tag: &AuthTag, data: &[u8]) -> bool {
-        auth::verify(tag, data, &self.0)
+    pub fn verify(&self, tag: [u8; 32], data: &[u8]) -> bool {
+        crypto_auth_verify(Uint8Array::from(&tag as &[u8]), data, &Uint8Array::from(&self.0 as &[u8]))
     }
 
     pub const fn size() -> usize {
-        size_of::<auth::Key>()
+        size_of::<[u8; 32]>()
     }
 }
 
 pub struct NonceGen {
-    next_nonce: secretbox::Nonce,
+    next_nonce: [u8; 24],
 }
 
 impl NonceGen {
-    pub fn new(pk: &handshake::EphPublicKey, net_id: &NetworkKey) -> NonceGen {
-        let hmac = auth::authenticate(&pk[..], &net_id.0);
-        const N: usize = size_of::<secretbox::Nonce>();
+    pub fn new(pk: &[u8; 32], net_id: &NetworkKey) -> NonceGen {
+        let mut hmac = [0u8; 32];
+        crypto_auth(&pk[..], &Uint8Array::from(&net_id.0 as &[u8])).copy_to(&mut hmac);
+        const N: usize = size_of::<[u8; 24]>();
         NonceGen {
-            next_nonce: secretbox::Nonce::from_slice(&hmac[..N]).unwrap(),
+            next_nonce: hmac[..N].try_into().unwrap(),
         }
     }
 
@@ -91,15 +106,15 @@ impl NonceGen {
     ///                      0, 0, 0, 0, 0, 0, 0, 0,
     ///                      0, 0, 0, 0, 0, 1, 0, 0]);
     /// ```
-    pub fn with_starting_nonce(nonce: secretbox::Nonce) -> NonceGen {
+    pub fn with_starting_nonce(nonce: [u8; 24]) -> NonceGen {
         NonceGen { next_nonce: nonce }
     }
 
-    pub fn next(&mut self) -> secretbox::Nonce {
+    pub fn next(&mut self) -> [u8; 24] {
         let n = self.next_nonce;
 
         // Increment the nonce as a big-endian u24
-        for byte in self.next_nonce.0.iter_mut().rev() {
+        for byte in self.next_nonce.iter_mut().rev() {
             *byte = byte.wrapping_add(1);
             if *byte != 0 {
                 break;
@@ -111,10 +126,12 @@ impl NonceGen {
 
 #[cfg(test)]
 mod tests {
-    use super::{generate_longterm_keypair, handshake::*, NetworkKey, PublicKey};
+    extern crate wasm_bindgen_test;
+    use wasm_bindgen_test::*;
+    use super::{generate_longterm_keypair, handshake::*, NetworkKey};
     use core::mem::size_of;
 
-    #[test]
+    #[wasm_bindgen_test]
     fn networkkey_random() {
         let a = NetworkKey::random();
         let b = NetworkKey::random();
@@ -126,20 +143,21 @@ mod tests {
         );
     }
 
-    #[test]
+    #[wasm_bindgen_test]
     fn shared_secret_with_zero() {
         let (c_eph_pk, _) = generate_ephemeral_keypair();
         let (c_pk, _) = generate_longterm_keypair();
+
 
         let (_, s_eph_sk) = generate_ephemeral_keypair();
         let (_, s_sk) = generate_longterm_keypair();
 
         assert!(derive_shared_secret(&s_eph_sk, &c_eph_pk).is_some());
-        let zero_eph_pk = EphPublicKey::from_slice(&[0; size_of::<EphPublicKey>()]).unwrap();
+        let zero_eph_pk = [0u8; 32];
         assert!(derive_shared_secret(&s_eph_sk, &zero_eph_pk).is_none());
 
         assert!(derive_shared_secret_pk(&s_eph_sk, &c_pk).is_some());
-        let zero_pk = PublicKey::from_slice(&[0; size_of::<PublicKey>()]).unwrap();
+        let zero_pk = &[0; size_of::<[u8; 32]>()];
         assert!(derive_shared_secret_pk(&s_eph_sk, &zero_pk).is_none());
 
         assert!(derive_shared_secret_sk(&s_sk, &c_eph_pk).is_some());
